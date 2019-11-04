35298252a0c15a4dc611237305b03800311a2f3e
FWSU <yys9905@naver.com>
2019-11-04 13:31:34 +0900

    MyMouseFrame & MyApp  - add description

--- MyMouseFrame.java
+++ MyMouseFrame.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 /**
- * Write a description of class MyFrame here.
+ * 레이블과 마우스리스너 마우스모션리스너를 포함하고 있는 
+ * 패널을 만들어 프레임에 넣어주는 클래스
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315036 양유석, 2018315030 이가영, 2018315022 이지연)
+ * @version (2019.11.04)
  */
 public class MyMouseFrame extends JFrame
 {
